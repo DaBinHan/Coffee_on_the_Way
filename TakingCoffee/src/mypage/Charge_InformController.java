264e4be88d31c4d635f9847873f3aa43cebd28cd
jinnyeong <jin3216@naver.com>
2017-12-25 23:27:37 +0900

    mypage_input_3

---
+++ TakingCoffee/src/mypage/Charge_InformController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Leejinnyeong
+ */
+public class Charge_InformController implements Initializable {
+
+    @FXML
+    private Label Label_Account;
+    @FXML
+    private Label Label_AccountNumber;
+    @FXML
+    private Label Label_We;
+    @FXML
+    private Label Label_OurName;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
